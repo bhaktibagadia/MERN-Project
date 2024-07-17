8148eddf47e454d00deee16a018a7c6508522808
Bhakti Bagadia <bagadiabhakti2@gmail.com>
2024-07-17 19:04:33 +0530

    done till contact

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -64,4 +64,17 @@ const login = async (req, res) => {
     }
 };
 
-module.exports={home, register, login};
+// to send user data
+
+const user = async (req, res) => {
+    try {
+        const userData = req.user;
+        console.log(userData);
+        return res.status(200).json({ userData });
+    } catch (error) {
+        console.log(`error from route ${error}`);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+module.exports={home, register, login, user};
